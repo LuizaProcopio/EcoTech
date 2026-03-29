f0a1f4ab78c39ecc999ce67a9c09d49ed809c6a6
LuizaProcopio <luiza.tavares.mp@gmail.com>
2026-03-29 19:33:01 -0300

    feat(login): cria tela de login (#1)

--- ecotech/lib/src/pages/home_page.dart
+++ ecotech/lib/src/pages/home_page.dart
@@ -1,9 +1,15 @@
 import 'package:ecotech/src/widgets/button_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:ecotech/src/pages/login_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,7 +67,12 @@ class HomePage extends StatelessWidget {
             // Botão de login
             ButtonWidget(
               texto: "LOGIN",
-              onClik: () {},
+              onClik: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+              },
             ),
           ],
         ),
